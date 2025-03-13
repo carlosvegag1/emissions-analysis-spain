c2da183aec01739cb2e497d9bee8eed39c693b4e
carlosvegag1 <carlosvegagonzalez1@gmail.com>
2025-03-13 13:41:59 +0100

    Resolviendo conflictos y actualizando

---
+++ requirements.R
@@ -0,0 +1,10 @@
+install.packages(c("dplyr", "ggplot2", "tidyr", "readr", "here", "rmarkdown", "knitr"))
+
+
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(readr)
+library(here)
+library(rmarkdown)
+library(knitr)
